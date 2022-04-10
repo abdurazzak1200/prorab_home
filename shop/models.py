364084da0387f038a2ff9e562e7c507f79de263f
abdurazzak1200 <toloev.abi99@gmail.com>
2022-04-10 07:01:33 +0600

    migrate models liter, diameter, meter

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 class Liter(models.Model):
+    liter = models.IntegerField('Литр')
+
+
+class Diameter(models.Model):
+    diameter = models.DecimalField('Диаметр', max_digits=3, decimal_places=1)
+
+
+class Meter(models.Model):
+    meter = models.DecimalField('Метр', max_digits=10, decimal_places=1)
