8affe09f07ce282ae202cc1ea8e3bea80d6e42cd
abdurazzak1200 <toloev.abi99@gmail.com>
2022-04-10 07:10:13 +0600

    add new fields liter, diameter, meter in model Product

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Liter, Diameter, Meter
 
 # Register your models here.
+admin.site.register(Liter)
+admin.site.register(Diameter)
+admin.site.register(Meter)
--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,28 @@ from django.db import models
 class Liter(models.Model):
     liter = models.IntegerField('Литр')
 
+    def __str__(self):
+        return f'{self.liter}'
+
+    class Meta:
+        verbose_name = 'Литр'
+
 
 class Diameter(models.Model):
     diameter = models.DecimalField('Диаметр', max_digits=3, decimal_places=1)
 
+    def __str__(self):
+        return f'{self.diameter}'
+
+    class Meta:
+        verbose_name = 'Диаметр'
+
 
 class Meter(models.Model):
     meter = models.DecimalField('Метр', max_digits=10, decimal_places=1)
+
+    def __str__(self):
+        return f'{self.meter}'
+
+    class Meta:
+        verbose_name = 'Метр'
