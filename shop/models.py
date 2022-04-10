944c42c7251f17a9a506183ac5583e99cb1bc06d
abdurazzak1200 <toloev.abi99@gmail.com>
2022-04-10 06:56:48 +0600

    start new models liter, diametr and meter

--- shop/models.py
+++ shop/models.py
@@ -1,2 +1,3 @@
 from django.db import models
 
+class Liter(models.Model):
--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', ShopView.as_view(), name='shop_list'),
-    path('shop/<str:subcategory_slug>/', ShopView.as_view(), name='subcategory_list'),
-    path('shop/<str:category_slug>/', ShopView.as_view(), name='category_list'),
+    path('<str:subcategory_slug>/', ShopView.as_view(), name='subcategory_list'),
+    path('<str:category_slug>/', ShopView.as_view(), name='category_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail')
 ]
