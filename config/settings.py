48b5bd4de1d96c2f61f106108985914567cbf018
abdurazzak1200 <toloev.abi99@gmail.com>
2022-06-08 15:41:23 +0600

    1321123

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'prorab-home/static'),os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
